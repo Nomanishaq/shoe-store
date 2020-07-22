9515337855ffaf2c90096cc6da0c5cbe0a6e5499
nomanishaq <nomanishaq251@gmail.com>
2020-07-23 02:57:57 +0500

    update

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import {Home,Product,SingleProduct,NotFound, Navbar, Footer} from './Components/InitializeComponents';
-import Context from './CountContext';
 
  function Router(){
 
